db45316b514d101c7e2c1b03c62d5b17dc17bc02
umar.m.peerboccus <umar.m.peerboccus@accenture.com>
2023-12-26 16:52:23 +0400

    Layout structure

---
+++ src/views/DisplayView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Task</th>
+      <th scope="col">Description</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Task1</th>
+      <td>Description</td>
+      <button type="button" class="btn btn-outline-primary">Edit</button>
+      <button type="button" class="btn btn-outline-primary">Complete</button>
+      <button type="button" class="btn btn-outline-primary">Delete</button>
+    </tr>
+    <tr>
+      <th scope="row">Task2</th>
+      <td>Description</td>
+      <button type="button" class="btn btn-outline-primary">Edit</button>
+      <button type="button" class="btn btn-outline-primary">Complete</button>
+      <button type="button" class="btn btn-outline-primary">Delete</button>
+    </tr>
+  </tbody>
+</table>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,32 @@
 <script setup lang="ts">
+import DisplayView from "./DisplayView.vue";
 </script>
 
-<template>Test</template>
+<template>
+    <div class="row">
+        <div class="col">
+            <div class="mb-3">
+                <label class="form-label">Title</label>
+                <input type="text" class="form-control" placeholder="Title">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Description</label>
+                <input type="text" class="form-control" placeholder="Description">
+            </div>
+            <div class="mb-3">
+                <button type="button" class="btn btn-outline-primary">Add</button>
+            </div>
+        </div>
+        <div class="col">
+            <div class="mb-3">
+                <label class="form-label">Remaining Items</label>
+                <input type="text" class="form-control" placeholder="Counter">
+            </div>
+        </div>
+        <div class="row">
+            <DisplayView/>
+        </div>
+    </div>
+</template>
 
 <style scoped></style>
