8579ccc07e6a3d8232e3f67c8a470756064fa3bf
umar.m.peerboccus <umar.m.peerboccus@accenture.com>
2023-12-27 22:51:32 +0400

    install bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
-
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import "bootstrap-icons/font/bootstrap-icons.css"
 const app = createApp(App)
 
 app.use(createPinia())
